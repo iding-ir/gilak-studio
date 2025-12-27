5764d0dcd9f3643815b166755664139547e4aa33
Aydin Ghane Kh. <aydin.ghane.kh@gmail.com>
2025-12-27 20:31:25 +0100

    Add size prop to tooltip

--- packages/gilak-components/src/components/Tooltip/Tooltip.tsx
+++ packages/gilak-components/src/components/Tooltip/Tooltip.tsx
@@ -1,7 +1,7 @@
 import "tippy.js/dist/tippy.css";
 import "tippy.js/dist/tippy.css";
 
-import type { Variant } from "@gilak/components/types";
+import type { TshirtSize, Variant } from "@gilak/components/types";
 import Tippy from "@tippyjs/react";
 import clsx from "clsx";
 import type { ReactElement } from "react";
@@ -11,6 +11,7 @@ import styles from "./Tooltip.module.scss";
 export interface TooltipProps {
   content: ReactElement | string;
   variant?: Variant;
+  size?: TshirtSize;
   delay?: number;
   className?: string;
   children: ReactElement;
@@ -19,6 +20,7 @@ export interface TooltipProps {
 export const Tooltip = ({
   content,
   variant = "secondary",
+  size = "xs",
   delay = 100,
   className,
   children,
@@ -27,7 +29,7 @@ export const Tooltip = ({
     <Tippy
       delay={delay}
       content={content}
-      className={clsx(styles.tooltip, styles[variant], className)}
+      className={clsx(styles.tooltip, styles[variant], styles[size], className)}
     >
       {children}
     </Tippy>
