eaf2ebc515a8699bf73e5d7d5f1f2ed29a5c3a3b
Aydin Ghane Kh. <aydin.ghane.kh@gmail.com>
2026-01-18 15:01:35 +0100

    Refactor Group component

--- packages/gilak-components/src/components/Group/Group.tsx
+++ packages/gilak-components/src/components/Group/Group.tsx
@@ -1,17 +1,18 @@
 import clsx from "clsx";
+import type { ReactNode } from "react";
 
 import styles from "./Group.module.scss";
 
 export type GroupProps = {
   title?: string;
   direction?: "row" | "column" | "rowReverse" | "columnReverse";
-  children?: React.ReactNode;
+  children?: ReactNode;
 };
 
 export const Group = ({ title, direction = "row", children }: GroupProps) => {
   return (
     <div className={clsx(styles.root, styles[direction])}>
-      {title && <h3 className={styles.title}>{title}</h3>}
+      {title && <h4 className={styles.title}>{title}</h4>}
       <div className={styles.content}>{children}</div>
     </div>
   );
